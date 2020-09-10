0346537261af0359371453579445dbe58d6b1dba
gatsjy <38937414+gatsjy@users.noreply.github.com>
2020-09-10 23:29:36 +0900

    2020-09-10, jpa 복원 및 jpql 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -12,20 +13,28 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
-
         tx.begin();
 
-        Member member = new Member();
+        try{
+            Member findMember = em.find(Member.class, 1L);
 
-        member.setId(1L);
-        member.setName("HelloA");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(1)
+                    .getResultList();
 
-        em.persist(member);
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
-        tx.commit();
+            findMember.setName("HelloJPA");
 
-        em.close();
-
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
 
     }
