645bf71ea9e9a1cad7f67f368eaf5f8f31abe251
gatsjy <38937414+gatsjy@users.noreply.github.com>
2020-09-11 20:41:43 +0900

    2020-09-11, 한주안, 1차캐시에 대한 설명

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,14 +19,20 @@ public class JpaMain {
 
             // 비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             // 영속
-            System.out.println("BEFORE");
-            em.persist(member);
-            em.detach(member);
-            System.out.println("Afrer");
+            System.out.println("==== BEFORE ===");
+            em.persist(member); // 여기서 1차 캐시에 저장
+            System.out.println("==== AFTER ===");
+
+            // 디비에서 가져 오는 것이 아니라 1차캐시에서 값을 가져온다.
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
