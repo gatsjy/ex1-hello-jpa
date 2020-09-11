0c4bfbc2de8d8dcbc422ae42a7ec3f88adb60b3e
gatsjy <38937414+gatsjy@users.noreply.github.com>
2020-09-11 20:47:02 +0900

    2020-09-11, 한주안, 영속 엔티티의 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,22 +16,12 @@ public class JpaMain {
         tx.begin();
 
         try{
-
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
             // 영속
-            System.out.println("==== BEFORE ===");
-            em.persist(member); // 여기서 1차 캐시에 저장
-            System.out.println("==== AFTER ===");
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
-            // 디비에서 가져 오는 것이 아니라 1차캐시에서 값을 가져온다.
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+            // 영속 엔티티의 동일성 보장
+            System.out.println(" result = " +  (findMember1 == findMember2));
 
             tx.commit();
         }catch (Exception e){
