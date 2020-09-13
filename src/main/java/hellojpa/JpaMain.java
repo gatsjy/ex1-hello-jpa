c01adedddedaaccb36a1ebe09738ab08b9ba5cfc
gatsjy <38937414+gatsjy@users.noreply.github.com>
2020-09-13 20:26:22 +0900

    2020-09-13, 한주안, 양방향 연관관계와 연관관계의 주인 1 - 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -26,11 +26,24 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
-            // 조회할때 이슈
-            Member findMember = em.find(Member.class, member.getId());
+            em.flush();
+            em.clear();
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            // 조회할때 이슈
+            // Member findMember = em.find(Member.class, member.getId());
+            // 이 코드는 멤버에서 팀으로 갈 수 있지만, 팀에서 멤버로는 갈 수 없다.
+            // 사실 둘은 서로 왔다 갔다 할 수 있다. -> 이러한 경우를 양방향 연관관계라 표현한다.
+            // 테이블의 경우에는 그저 포린키가 들어있으면 서로의 연관관계가 매핑이 된다.
+            // 하지만 중요한 사실은 객체의 경우이다. 객체의 경우 서로의 연관관계를 매핑시켜줄 어떠한 코딩을 해야한다.
+            // Team findTeam = findMember.getTeam();
+            // System.out.println("findTeam = " + findTeam.getName());
+            // 반대 방향으로도 객체 그래프 탐색을 할 수 있다.
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,6 +11,18 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    // 멤버에서 팀으로 -> 다대일
+    // 팀에서 멤버로 -> 일대다
+    // mappedBy 키워는 반대편 방향의 team 이라는 멤버변수를 매칭 시킨다.
+    // *mappedBy 의 정체는? -> 객체와 테이블간에 연관관계를 맺는 차이를 이해해야 한다.
+    // 객체 연관관계 = 2개
+    // 회원 -> 팀 연관관계 1개(단방향)
+    // 팀 -> 회원 연관관계 1개(단방향)
+    // 테이블 연관관계 = 1개
+    // 회원 <-> 팀의 연관관계 1개(양방향)
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -27,5 +38,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
