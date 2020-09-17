b1ca4cdf0c806a08a318953a6abfa8d5d71e6370
gatsjy <38937414+gatsjy@users.noreply.github.com>
2020-09-17 21:47:59 +0900

    2020-09-17, 한주안, 상속관계 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,16 +16,19 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = new Member();
-            member.setUsername("member1");
+            Movie movie = new Movie();
+            movie.setDirector("aaa");
+            movie.setActor("bbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
 
-            em.persist(member);
+            em.persist(movie);
+            
+            em.flush();
+            em.clear();
 
-            Team team = new Team();
-            team.setName("teamA");
-
-            team.getMembers().add(member);
-            em.persist(team);
+            Item findMovie = em.find(Item.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
 
